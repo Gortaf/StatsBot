c8a9a336ceea05b49c7dbbca799556c0a50c2cc4
Gortaf <36972435+Gortaf@users.noreply.github.com>
2020-11-19 10:32:54 -0500

    Added new commands
    -added serverstats
    -added the roles serverstats type
    -added an help command

--- StatsBot.py
+++ StatsBot.py
@@ -20,6 +20,7 @@ from dotenv import load_dotenv
 import matplotlib
 import matplotlib.pyplot as plt
 import random as rn
+import string
 
 from discord.ext import commands
 from discord.utils import get
@@ -31,9 +32,10 @@ from datetime import date
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
-server = os.getenv('DISCORD_SERVER')
 
 client =commands.Bot(command_prefix = ">")
+client.remove_command('help')
+client.suggestOpened={}
 
 @client.event
 async def on_ready():
@@ -42,7 +44,7 @@ async def on_ready():
 @client.command(pass_context=True)
 async def poll(ctx,arg,arg2=30):
 	
-	#Function to not display unvoted option in the pie chart
+	#Not displaying unvoted option in the pie chart
 	def spe_autopct(pct):
 		return ('%.2f%%' % pct) if pct != 0 else ''
 	
@@ -53,22 +55,19 @@ async def poll(ctx,arg,arg2=30):
 	
 	#Format the args
 	choices = arg.split(",")
-	
-	#Check if there are enough arguments
-	if arg == None or len(choices) < 2:
-		ctx.send("You need at least 2 option to start a poll.")
-		return
-	
+		
 	#Number emotes. Index corresponds with emote
 	emoteRef = ["0️⃣","1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"]
 	
-	print(arg)
-	
-	#Check if there aren't too many args
-	if len(choices) > 10:
-		ctx.send("You cannot add more than 10 choices")
+	#Check if there are enough arguments
+	if arg == None or len(choices) < 2:
+		await ctx.send("You need at least 2 option to start a poll.")
 		return
 	
+	#Check if there aren't too many args
+	elif len(choices) > 10:
+		await ctx.send("You cannot add more than 10 choices")
+		return
 	
 	#Create the message
 	toPrint = "Please choose between: "
@@ -123,7 +122,7 @@ async def poll(ctx,arg,arg2=30):
 	plt.clf()   # Clear figure
 	plt.close()
 
-@client.command(pass_context=True)
+@client.command(pass_context=True) # TODO: rewrite with *args
 async def userstats(ctx, statType = "help",user = None, private = None):
 	
 	if private == "private":
@@ -214,10 +213,6 @@ async def userstats(ctx, statType = "help",user = None, private = None):
 		# List of nb of messages/channel ordered by the channel list
 		for channel in uniqueChannel:
 			channelNb.append(channelList.count(channel))
-		
-# 		print("dayMonthList = "+ str(dayMonthList))
-# 		print("dayNb = "+str(dayNb))
-# 		print("uniqueDayMonth = "+ str(uniqueDayMonth))
 
 		# Creates the msg/day graph
 		matplotlib.rcParams.update({'font.size': 14})
@@ -263,16 +258,63 @@ async def userstats(ctx, statType = "help",user = None, private = None):
 		plt.clf()   # Clear figure
 		plt.close()
 					
-	await eval(f"{statType}(ctx,user,{private})")
-	
-	
-@client.command(pass_context=True)
-async def open_suggestions(ctx, title = "Suggestions", time = 600):
-	client.suggest_opened = True
-	await asyncio.sleep(time)
-	client.suggest_opened = False
-	
+	await eval(f"{statType}(ctx,user,private)")
 
+@client.command(pass_context=True)
+async def serverstats(ctx, *args):
 	
+	if args[len(args)-1] == "private":
+		private = True
+	else:
+		private = False
+		
+	#Not displaying unvoted option in the pie chart
+	def spe_autopct(pct):
+		return ('%.2f%%' % pct) if pct != 0 else ''
+	
+	async def channels(ctx, private):
+		pass
+		
+	async def roles(ctx, private):
+		tmp = ctx.guild.roles
+		dicRoles = {toAdd:0 for toAdd in tmp if not toAdd.is_default()}
+		sizes = []
+		
+		for toCheck in ctx.guild.members:
+			for toAdd in toCheck.roles:
+				if not toAdd.is_default():
+					dicRoles[toAdd] += 1
+					
+		sizes = dicRoles.values()
+		
+		#Create the pie chart
+		patches, texts, percent = plt.pie(sizes, shadow=True, startangle=140, autopct=spe_autopct)
+		plt.legend(patches, dicRoles.keys(), loc="best", title=f"Role repartition in {ctx.guild.name}")
+		plt.axis('equal')
+		plt.tight_layout()
+	
+		#Randomly generated plot ID to prevent mixing up plots between users
+		plotID = str(rn.randrange(1,100000))
+		plt.savefig(f"Plot_id{plotID}.png", transparent=True)
+		
+		#Send the pie chart
+		await ctx.send(file=discord.File(f'Plot_id{plotID}.png'))
+		os.remove(f'Plot_id{plotID}.png')
+		
+		plt.cla()   # Clear axis
+		plt.clf()   # Clear figure
+		plt.close()
+		
+		print(dicRoles)
+		
+		
+	await eval(f"{args[0]}(ctx, private)")
+
+
+@client.command(pass_context=True)
+async def help(ctx, *args):
+	user = ctx.author
+	toSend = " ```\nThis is the list of the currently available commands:        ([argument] = required argument, {argument} = optional argument)\n\n>help\nSends this message in the user's DMs.\n\n>poll [options] {time}\nThis command will generate a poll for users to vote. At the of the time, the bot will post a pie chart of the results.\n[options]: All the options the users can vote for. Follow this format: \"option1,option2,option3\" (quotation marks included). You can add up to 10 options.\n{time}: The amount of time users will have to vote in seconds. If no time is specified, defaults to 30.\n# Note: If the bots needs to restart, ongoing polls will be lost. Try to avoid making the poll too long to prevent any kind of loss.\n\n>userstats [type] {@user} {private}\nThis command allows you to obtain various stats on a user. \n[type]: There are several types:\nmessages: this will retrieve the messages from a user, and post graphs of the user's messaging history statistics on this server.\n(more types to come)\n{@user}: The user you wish to collect stats from. You need to mention the user with @user. If no user is specified, defaults to the one who used the command.\n{private}: if the keyword \"private\" is used at the end of the command, the results will be sent in the DMs of the person using the command.\n\n>serverstats [type] {private}\nThis command allows you to obtain various stats on the server.\n[type]: There are several types:\nroles: This will retrieve the repartition of roles in the server and post a pie chart showing it.\n(more types to come)\n{private}: if the keyword \"private\" is used at the end of the command, the results will be sent in the DMs of the person using the command.\n```"
+	await user.send(toSend)
 
 client.run(token)
